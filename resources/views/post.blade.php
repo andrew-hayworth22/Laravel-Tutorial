706669e5b3383552bbc82260a58aecb6fbd77c90
Andy Hayworth <mhaywort@kent.edu>
2022-04-18 22:13:38 -0400

    Initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<link rel = "stylesheet" href = "/app.css">
+{{-- <script src = "/app.js"> --}}
+
+<title>My Blog</title>
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+
+    <a href = "/">Go Back</a>
+</body>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(!file_exists($path)) {
+        // dd("File doesn't exist");
+        // ddd("File doesn't exist");
+        // abort(404);
+        return redirect("/");
+    }
+
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), function() use($path) {
+        // var_dump("Caching file");
+        return file_get_contents($path);
+    });
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+'); // also whereAlpha(), whereAlphanumeric(), whereNumber()
