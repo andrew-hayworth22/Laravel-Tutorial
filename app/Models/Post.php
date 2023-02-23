85f447a136426b26fec703522267bcdac34fe310
Andy Hayworth <mhaywort@kent.edu>
2023-02-22 21:38:00 -0500

    Some code cleanup

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,6 @@ class Post extends Model
 
     protected $guarded = ['id'];
     protected $fillable = ['title'];
-
     protected $with = ['category', 'author'];
 
     public function scopeFilter($query, array $filters) {
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,33 +51,7 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    @auth
-                        <form method="post" action="/posts/{{ $post->slug }}/comments">
-                            <x-panel>
-                                @csrf
-                                <header class="flex items-center">
-                                    <img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" class="rounded-full">
-                                    <h2 class="ml-4">Want to Participate?</h2>
-                                </header>
-
-                                <div class="mt-6">
-                                    <textarea
-                                        name="body"
-                                        class="w-full text-sm focus:outline-none focus:ring"
-                                        rows="5"
-                                        placeholder="Quick, think of something to say!"></textarea>
-                                </div>
-
-                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200" >
-                                    <button type="submit" class="bg-blue-500 text-white font-semibold uppercase text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                                </div>
-                            </x-panel>
-                        </form>
-                    @else
-                        <p>
-                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Login</a> to leave a comment
-                        </p>
-                    @endauth
+                    @include('posts._add-comment-form')
 
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment"/>
